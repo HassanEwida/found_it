b17fbba8c015c4f2a1c3f621aa96ba72530be9eb
HassanEwida <ewida6@gmail.com>
2024-06-17 14:52:50 +0300

    more modifications to the last features

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,23 @@
 import React from "react";
 import FoundIt from './found_it_logo.png';
 import { Tilt } from 'react-next-tilt';
+import { Link } from "react-router-dom";
 import './Header.css';
 
-const Logo = () => {
-    return(
-        <header className="center w-100" style={{paddingTop: 10, paddingBottom: 50,}}>
-            <Tilt options={{ max: 25, scale: 1.1 }} preserve3dEnable={true} className="tilt-container">
-                <div className='tilt-box'>
-                    <img src={FoundIt} alt="logo" height='auto' width='250'/>
-                </div>
-            </Tilt>
-        </header>
-    );
+const Logo = ({ path }) => {
+    if(path !== "/Login" || path !== "/Register"){
+        return(
+            <Link to={"/home"}>
+            <header className="center w-100" style={{paddingTop: 10, paddingBottom: 50,}}>
+                <Tilt options={{ max: 25, scale: 1.1 }} preserve3dEnable={true} className="tilt-container">
+                    <div className='tilt-box'>
+                        <img src={FoundIt} alt="logo" height='auto' width='250'/>
+                    </div>
+                </Tilt>
+            </header>
+            </Link>
+        );
+    }
 }
 
 export default Logo;
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 
 
@@ -7,32 +8,37 @@ const Register = () => {
     const { register, handleSubmit } = useForm();
     const [ newRegister, setNewRegister ] = useState("");
     return(
-        <div className="pa5">
-            <h1 className="f1">Register</h1>
-            <form onSubmit={handleSubmit((newRegister) => setNewRegister(JSON.stringify(newRegister)))} style={{textAlign: 'left', margin: 'auto', width: '100%', maxWidth: '800px'}}>
-                <div className="input-wrapper">
-                    <input {...register("username", {required: true})} type="email" className="input-field" />
-                    <span className="input-label">Email:</span>
-                    <span className="input-shadow"></span>
-                </div>
-                <div className="input-wrapper">
-                    <input {...register("username", {required: true})} className="input-field" />
-                    <span className="input-label">Username:</span>
-                    <span className="input-shadow"></span>
-                </div>
-                <div className="input-wrapper">
-                    <input {...register("password", {required: true})} type="password" className="input-field" />
-                    <span className="input-label">Password:</span>
-                    <span className="input-shadow"></span>
-                </div>
-                <div className="input-wrapper">
-                    <input {...register("confirmPassword", {required: true})} type="password" className="input-field" />
-                    <span className="input-label">Confirm Password:</span>
-                    <span className="input-shadow"></span>
-                </div>
-                <p>{console.log(newRegister)}</p>
-                <button type="submit" className="button-52">Register</button>
-            </form>
+        <div>
+            <Link to="/">
+                <nav style={{display: 'flex', position: 'start', marginLeft: '30px'}}><button className="button-52">Back</button></nav>
+            </Link>
+            <div className="pa5">
+                <h1 className="f1">Register</h1>
+                <form onSubmit={handleSubmit((newRegister) => setNewRegister(JSON.stringify(newRegister)))} style={{textAlign: 'left', margin: 'auto', width: '100%', maxWidth: '800px'}}>
+                    <div className="input-wrapper">
+                        <input {...register("username", {required: true})} type="email" className="input-field" />
+                        <span className="input-label">Email:</span>
+                        <span className="input-shadow"></span>
+                    </div>
+                    <div className="input-wrapper">
+                        <input {...register("username", {required: true})} className="input-field" />
+                        <span className="input-label">Username:</span>
+                        <span className="input-shadow"></span>
+                    </div>
+                    <div className="input-wrapper">
+                        <input {...register("password", {required: true})} type="password" className="input-field" />
+                        <span className="input-label">Password:</span>
+                        <span className="input-shadow"></span>
+                    </div>
+                    <div className="input-wrapper">
+                        <input {...register("confirmPassword", {required: true})} type="password" className="input-field" />
+                        <span className="input-label">Confirm Password:</span>
+                        <span className="input-shadow"></span>
+                    </div>
+                    <p>{console.log(newRegister)}</p>
+                    <button type="submit" className="button-52">Register</button>
+                </form>
+            </div>
         </div>
     )
 }
